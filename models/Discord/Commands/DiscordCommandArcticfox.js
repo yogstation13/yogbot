41a067ef5a5ee0f816095ac7dd1334317614f1c3
Nichlas Pihl <nichlas0010@gmail.com>
2017-06-07 18:13:22 +0000

    fixd

--- models/Discord/Commands/DiscordCommandArcticfox.js
+++ models/Discord/Commands/DiscordCommandArcticfox.js
@@ -19,4 +19,8 @@ class DiscordCommandArcticfox extends DiscordCommand {
 
 }
 
+<<<<<<< HEAD
 module.exports = DiscordCommandArcticfox;
+=======
+module.exports = DiscordCommandArcticfox;
+>>>>>>> fixd
