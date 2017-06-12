0e7f3c16ca902e765dcb6b211dc4600a6b2f4352
Nichlas Pihl <Nichlas0010@gmail.com>
2017-06-12 10:47:55 +0200

    Prevents people from kicking themselves using !kick

--- models/Discord/Commands/DiscordCommandKick.js
+++ models/Discord/Commands/DiscordCommandKick.js
@@ -6,50 +6,55 @@ class DiscordCommandKick extends DiscordCommand {
     super("kick", "Kick a user.", 'kick', subsystem);
   }
 
-  onRun(message, permissions, args) {
-    var config = this.subsystem.manager.getSubsystem("Config").config;
-    if(args.length < 1) {
-      message.reply("Usage is `" + config.discord_command_character + "kick [@UserName] <reason>`");
-    }
-
-    var user = undefined;
-
-    for (var auser of message.mentions.users.array()) {
-      user = auser;
-      break;
-    }
-
-    if(user == undefined) {
-      message.reply("I could not find that user, Make sure you use the mention format of @Username");
-      return;
-    }
-
-
-    var guildMember = message.guild.fetchMember(user);
-    guildMember.then(
-      function(resolve) {
-        args.shift();
-
-        var reason = "No reason given.";
-
-        if(args.length > 0) {
-          reason = args.join(" ");
+    onRun(message, permissions, args) {
+        var config = this.subsystem.manager.getSubsystem("Config").config;
+        if(args.length < 1) {
+            message.reply("Usage is `" + config.discord_command_character + "kick [@UserName] <reason>`");
         }
 
-        resolve.kick(0).then(
-          function(resolve) {
-            message.reply(resolve.user.username + " Was kicked from the server for `" + reason + "`.");
+        var user = undefined;
+
+        for (var auser of message.mentions.users.array()) {
+            user = auser;
+            break;
+        }
+
+        if(user == undefined) {
+            message.reply("I could not find that user, Make sure you use the mention format of @Username");
+            return;
+        }
+
+
+        var guildMember = message.guild.fetchMember(user);
+        guildMember.then(
+          function (resolve) {
+              if (resolve.id == message.member.id) {
+                  message.reply("You cannot kick yourself");
+              }
+              else {
+                  args.shift();
+
+              var reason = "No reason given.";
+
+              if (args.length > 0) {
+                  reason = args.join(" ");
+              }
+
+              resolve.kick(0).then(
+                function (resolve) {
+                    message.reply(resolve.user.username + " Was kicked from the server for `" + reason + "`.");
+                },
+                function (reject) {
+                    message.reply("An error occured.");
+                }
+              );
+            }
           },
-          function(reject) {
-            message.reply("An error occured.");
+          function (reject) {
+              message.reply("I could not find that user, Make sure that member is part of this guild.");
           }
         );
-      },
-      function(reject) {
-        message.reply("I could not find that user, Make sure that member is part of this guild.");
-      }
-    );
-  }
+    }
 
 }
 
