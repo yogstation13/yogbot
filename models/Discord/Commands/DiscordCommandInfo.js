8502d11db119b2c9ff210f137018ee9b30142045
nichlas0010 <nichlas00100@gmail.com>
2017-11-21 18:34:04 +0100

    Adds round duration to !info

--- models/Discord/Commands/DiscordCommandInfo.js
+++ models/Discord/Commands/DiscordCommandInfo.js
@@ -26,28 +26,49 @@ class DiscordCommandInfo extends DiscordCommand {
 				if ('error' in results) {
 					return message.reply(results.error);
 				}
-				var embedcolor = "";
-				var colors = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+				byondSS.byondConnector.request("?status", (resultsstatus) => {
+					if ('error' in resultsstatus) {
+						return message.reply(results.error);
+					}
+					var statusarray = resultsstatus.data.split("&");
+					var statusarray2 = [];
+					for(var i = 0; i < 18; i++) {
+						var keyandvalue = statusarray[i];
+						var valuearray = keyandvalue.split("=");
+						if(valuearray[0] == "real_mode") {
+							continue
+						}
+						var value = valuearray[1];
+						statusarray2.push(value);
+						//for a list of all the values, either uncomment the line below this or check world.dm in the game repo
+						//console.log(valuearray)
+					}
+					var round_duration = statusarray2[15];
+					round_duration = Math.round(round_duration/60);
+					var embedcolor = "";
+					var colors = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
 
-				for(var count = 0; count < 6; count++) {
+					for(var count = 0; count < 6; count++) {
 						embedcolor = embedcolor + colors[Math.floor(Math.random() * colors.length)];
 						//picks a colour from the array "colours" then proceeds to add it to "colour", and stops once "colour" has 6 digits
-				}
-				if(adminwho == "\n") {
-					adminwho = "No admins online";
-				}
+					}
+					if(adminwho == "\n") {
+						adminwho = "No admins online";
+					}
 
-				var embed = new Discord.RichEmbed();
+					var embed = new Discord.RichEmbed();
 
-				embed.setAuthor("Information", "http://i.imgur.com/GPZgtbe.png");
-				embed.setDescription("Join the server now by using " + config.server_join_address);
-				embed.addField("Players online:", results.data, true);
-				embed.addField("Current round:", byondSS.roundNumber, true);
-				embed.addField("Admins online:", adminwho, false);
-				embed.setColor(embedcolor);
+					embed.setAuthor("Information", "http://i.imgur.com/GPZgtbe.png");
+					embed.setDescription("Join the server now by using " + config.server_join_address);
+					embed.addField("Players online:", results.data, true);
+					embed.addField("Current round:", byondSS.roundNumber, true);
+					embed.addField("Round duration:", round_duration + " Minutes", true);
+					embed.addField("Admins online:", adminwho, false);
+					embed.setColor(embedcolor);
 
-				var channel = config.discord_public_channel;
-				message.channel.send({embed});
+					var channel = config.discord_public_channel;
+					message.channel.send({embed});
+				});
 			});
 		});
 	}
