68f5efc4cb73c5adf5be8c3953a63754211a2ac2
Nich <nichlas00100@gmail.com>
2018-01-23 14:11:52 +0100

    Allows toggling server OOC from discord

---
+++ models/Discord/Commands/DiscordCommandToggleOOC.js
@@ -0,0 +1,24 @@
+const DiscordCommand = require('../DiscordCommand.js');
+
+class DiscordCommandToggleOOC extends DiscordCommand {
+
+  constructor(subsystem) {
+    super("toggleooc", "Toggles server OOC.", 'toggleooc', subsystem);
+  }
+
+  onRun(message, permissions, args) {
+    var config = this.subsystem.manager.getSubsystem("Config").config;
+    var byondConnector = this.subsystem.manager.getSubsystem("Byond Connector").byondConnector;
+
+    byondConnector.request("?toggleooc", (results) => {
+      if('error' in results) {
+        message.reply(results.error);
+      } else {
+        message.reply(results.data);
+      }
+    });
+  }
+
+}
+
+module.exports = DiscordCommandToggleOOC;
