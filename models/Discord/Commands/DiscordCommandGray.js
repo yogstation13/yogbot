6e422c574e5ae035e5c2c6fed08fffe7a6a24f26
Nich <nichlas00100@gmail.com>
2018-03-13 22:21:56 +0100

    gray

--- models/Discord/Commands/DiscordCommandGray.js
+++ models/Discord/Commands/DiscordCommandGray.js
@@ -3,19 +3,19 @@ const DiscordCommand = require('../DiscordCommand.js');
 class DiscordCommandGray extends DiscordCommand {
 
   constructor(subsystem) {
-    super("gray", "Pictures of disappointment", undefined, subsystem, true);
+    super("gray", "أمة أوتاكو", undefined, subsystem, true);
   }
 
   onRun(message, permissions, args) {
-    var responses = [
+    var ردود = [
 		"https://i.imgur.com/NqAej6K.jpg",
 		"https://i.imgur.com/MMioT7U.jpg",
 		"https://i.imgur.com/hjFFSo0.jpg",
 		"https://i.imgur.com/viQe71t.jpg",
 		"https://i.imgur.com/KHrTewi.jpg"
 	];
-    var رد = responses[Math.floor(Math.random() * responses.length)];
-    message.reply(رد);
+    var responses = ردود[Math.floor(Math.random() * ردود.length)]; // I TRIED FOR SO LONG TO GET IT TO USE THE ARABIC FOR RESPONSES, BUT IT DECIDED THAT IT'D RATHER MOVE MY CODE AROUND AAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    message.reply(responses);
   }
 
 }
