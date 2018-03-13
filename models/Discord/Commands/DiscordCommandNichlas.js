21557d23bb83d0320668f23ad95062e124f5cbc3
Nich <nichlas00100@gmail.com>
2018-03-13 22:03:20 +0100

    det er jeg også

--- models/Discord/Commands/DiscordCommandNichlas.js
+++ models/Discord/Commands/DiscordCommandNichlas.js
@@ -3,17 +3,17 @@ const DiscordCommand = require('../DiscordCommand.js');
 class DiscordCommandNichlas extends DiscordCommand {
 
   constructor(subsystem) {
-    super("nichlas", "Something something potato", undefined, subsystem, true);
+    super("nichlas", "et-ellerandet et-eller-andet kartoffel", undefined, subsystem, true);
   }
 
   onRun(message, permissions, args) {
-    var responses = [
+    var svarene = [
       "http://i.imgur.com/RA0v3cW.jpg",
       "http://i.imgur.com/ugbj0m1.png",
       "http://i.imgur.com/pmgSHjz.png"
     ];
-    var response = responses[Math.floor(Math.random() * responses.length)];
-    message.reply(response);
+    var svar = svarene[Math.floor(Math.random() * svarene.length)];
+    message.reply(svar);
   }
 
 }
