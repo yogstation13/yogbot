c61fb88189ac6eb0df230cbb472f0ecd3d18f985
Nich <nichlas00100@gmail.com>
2018-06-18 15:12:15 +0200

    Update DiscordCommandAdminWho.js

--- models/Discord/Commands/DiscordCommandAdminWho.js
+++ models/Discord/Commands/DiscordCommandAdminWho.js
@@ -10,12 +10,12 @@ class DiscordCommandAdminWho extends DiscordCommand {
   onRun(message, permissions, args) {
     var config = this.subsystem.manager.getSubsystem("Config").config;
     var byondConnector = this.subsystem.manager.getSubsystem("Byond Connector").byondConnector;
-		var byondmessage = "?adminwho";
+    var byondmessage = "?adminwho";
 
-		if(message.channel.id == "134722036353204224" || message.channel.id == "378318242906636288" || message.channel.id == "134722015503319040") { //admin, admemes and council respectively
-			byondmessage += "&adminchannel=1"
- 
-		byondSS.byondConnector.request(byondmessage, (resultsadmin) => {
+    if(message.channel.id == "134722036353204224" || message.channel.id == "378318242906636288" || message.channel.id == "134722015503319040") { //admin, admemes and council respectively
+      byondmessage += "&adminchannel=1"
+    }
+    byondSS.byondConnector.request(byondmessage, (resultsadmin) => {
       if('error' in results) {
         message.reply(results.error);
       } else {
