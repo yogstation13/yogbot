ad5a2f76424e49d71a5c5b0033a2340e58aafd26
Ling <lingbleed@gmail.com>
2018-08-09 09:51:58 +0200

    Update DiscordCommandNotes.js

--- models/Discord/Commands/DiscordCommandNotes.js
+++ models/Discord/Commands/DiscordCommandNotes.js
@@ -32,9 +32,10 @@ class DiscordCommandNotes extends DiscordCommand {
         else {
           var msg = "Notes for " + ckey + "\n";
           for(var i = 0; i < results.length; i++){
-            msg += results[i].timestamp + "   " + results[i].text;
+            var result = results[i]
+            msg += result.timestamp + "   " + result.text;
             if(message.channel.id == config.discord_channel_admin || message.channel.id == config.discord_channel_admemes || message.channel.id == config.discord_channel_council) {
-              msg += "   " + results[i].adminckey;
+              msg += "   " + result.adminckey;
             }
             msg += "\n";
           }
