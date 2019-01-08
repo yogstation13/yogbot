baa4622dc96116e0fe96b0937c97e393222ad65c
nichlas0010 <nichlas00100@gmail.com>
2019-01-08 16:39:41 +0100

    Update DiscordCommandNotes.js

--- models/Discord/Commands/DiscordCommandNotes.js
+++ models/Discord/Commands/DiscordCommandNotes.js
@@ -17,7 +17,7 @@ class DiscordCommandNotes extends DiscordCommand {
     var ckey = args[0];
     var punctuation = [".", ",", "-", "_", ";", ":", " "];
     for(var i = 0; i< punctuation.length; i++) {
-      ckey = ckey.replace(punctuation[i], "");
+      ckey = ckey.split(punctuation[i]).join("");
     }
     
     var dbSubsystem = this.subsystem.manager.getSubsystem("Database");
