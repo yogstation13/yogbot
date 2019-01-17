f0cb158e894bf6dd6e4ecb7f8243b28074376e56
nichlas0010 <nichlas00100@gmail.com>
2019-01-17 03:44:08 +0100

    Update DiscordCommandReview.js

--- models/Discord/Commands/DiscordCommandReview.js
+++ models/Discord/Commands/DiscordCommandReview.js
@@ -37,171 +37,172 @@ class DiscordCommandReview extends DiscordCommand {
         message.reply("Error contacting database, try again later.");
       }
       while(ckeys2Check.length || ips2Check.length || cids2Check.length) {
-				var ckey2check;
-				var ip2check;
-				var cid2check;
+        var ckey2check;
+        var ip2check;
+        var cid2check;
         if(ckeys2Check.length) {
-				
-					ckey2check = ckeys2Check[0];
-					connection.query('SELECT * FROM `erro_connection_log` WHERE `ckey` = ? ', [ckey2check], (error, results, fields) => {
+          
+	  ckey2check = ckeys2Check[0];
+	  connection.query('SELECT * FROM `erro_connection_log` WHERE `ckey` = ? ', [ckey2check], (error, results, fields) => {
 
-						for(var/i = 0; i < results.length; i++) {
-							var result = results[i];
+	    for(var/i = 0; i < results.length; i++) {
+	      var result = results[i];
 							
-							//let's check the IPs first.
-							var alreadyCheckingIP = 0;
-							for(var/j = 0; j < ips2Check.length; j++) {
-								if(ips2Check[j] == result.ip) {
-									alreadyCheckingIP = 1;
-									break;
-								}
-							}
-							for(var/j = 0; j < checkedIPs.length; j++) {
-								if(checkedIPs[j] == result.ip) {
-									alreadyCheckingIP = 1;
-									break;
-								}
-							}
-							if(alreadyCheckingIP == 0) {
-								ips2Check.push(result.ip);
-							}
+	      //let's check the IPs first.
+	      var alreadyCheckingIP = 0;
+	      for(var j = 0; j < ips2Check.length; j++) {
+	        if(ips2Check[j] == result.ip) {
+		  alreadyCheckingIP = 1;
+		  break;
+		}
+	      }
+	      for(var j = 0; j < checkedIPs.length; j++) {
+		if(checkedIPs[j] == result.ip) {
+		  alreadyCheckingIP = 1;
+		  break;
+		}
+	      }
+	      if(alreadyCheckingIP == 0) {
+		ips2Check.push(result.ip);
+	      }
 							
-							//now let's check CIDs
-							var/alreadyCheckingCID = 0;
-							for(var/j = 0; j < cids2check.length; j++) {
-								if(cids2check[j] == result.computerid) {
-									alreadyCheckingCID = 1;
-									break;
-								}
-							}
-							for(var/j = 0; j < checkedCIDs.length; j++) {
-								if(checkedCIDs[j] == result.computerid) {
-									alreadycheckingCID = 1;
-								}
-							}
-							if(alreadyCheckingCID == 0) {
-								cids2Check.push(result.computerid);
-							}
+	      //now let's check CIDs
+	      var alreadyCheckingCID = 0;
+	      for(var/j = 0; j < cids2check.length; j++) {
+		if(cids2check[j] == result.computerid) {
+		  alreadyCheckingCID = 1;
+		  break;
+		}
+	      }
+	      for(var/j = 0; j < checkedCIDs.length; j++) {
+		if(checkedCIDs[j] == result.computerid) {
+		  alreadycheckingCID = 1;
+                  break;
+		}
+	      }
+	      if(alreadyCheckingCID == 0) {
+	        cids2Check.push(result.computerid);
+	      }
 							
-						}
+	    }
 						
-					});
-					checkedCkeys.push(ckey2check);
-					ckeys2Check.splice(0, 1);
+          });
+	  checkedCkeys.push(ckey2check);
+	  ckeys2Check.splice(0, 1);
 				
-				}
+	}
         if(ips2Check.length) {
 				
-					ip2check = ips2Check[0];
-					connection.query('SELECT * FROM `erro_connection_log` WHERE `ip` = ? ', [ip2check], (error, results, fields) => {
+	  ip2check = ips2Check[0];
+	  connection.query('SELECT * FROM `erro_connection_log` WHERE `ip` = ? ', [ip2check], (error, results, fields) => {
 
-						for(var/i = 0; i < results.length; i++) {
-							var result = results[i];
+	    for(var/i = 0; i < results.length; i++) {
+	      var result = results[i];
 							
-							//let's check the ckeys first.
-							var alreadyCheckingCkey = 0;
-							for(var/j = 0; j < ckeys2Check.length; j++) {
-								if(ckeys2Check[j] == result.ckey) {
-									alreadyCheckingCkey = 1;
-									break;
-								}
-							}
-							for(var/j = 0; j < checkedCkeys.length; j++) {
-								if(checkedCkeys[j] == result.ckey) {
-									alreadyCheckingCkey = 1;
-									break;
-								}
-							}
-							if(alreadyCheckingCkey == 0) {
-								ckeys2Check.push(result.ckey);
-							}
+	      //let's check the ckeys first.
+	      var alreadyCheckingCkey = 0;
+              for(var j = 0; j < ckeys2Check.length; j++) {
+	        if(ckeys2Check[j] == result.ckey) {
+		  alreadyCheckingCkey = 1;
+		  break;
+	        }
+	      }
+	      for(var j = 0; j < checkedCkeys.length; j++) {
+	        if(checkedCkeys[j] == result.ckey) {
+		  alreadyCheckingCkey = 1;
+		  break;
+	        }
+	      }
+	      if(alreadyCheckingCkey == 0) {
+	        ckeys2Check.push(result.ckey);
+	      }
 							
-							//now let's check CIDs
-							var/alreadyCheckingCID = 0;
-							for(var/j = 0; j < cids2check.length; j++) {
-								if(cids2check[j] == result.computerid) {
-									alreadyCheckingCID = 1;
-									break;
-								}
-							}
-							for(var/j = 0; j < checkedCIDs.length; j++) {
-								if(checkedCIDs[j] == result.computerid) {
-									alreadycheckingCID = 1;
-									break;
-								}
-							}
-							if(alreadyCheckingCID == 0) {
-								cids2Check.push(result.computerid);
-							}
+	      //now let's check CIDs
+	      var alreadyCheckingCID = 0;
+	      for(var j = 0; j < cids2check.length; j++) {
+	        if(cids2check[j] == result.computerid) {
+		  alreadyCheckingCID = 1;
+		  break;
+	        }
+	      }
+	      for(var/j = 0; j < checkedCIDs.length; j++) {
+	        if(checkedCIDs[j] == result.computerid) {
+	          alreadycheckingCID = 1;
+		  break;
+	        }
+	      }
+	      if(alreadyCheckingCID == 0) {
+	        cids2Check.push(result.computerid);
+	      }
 							
-						}
+	    }
 						
-					});
-					checkedIPs.push(ips2Check);
-					ips2Check.splice(0, 1);
+	  });
+	  checkedIPs.push(ips2Check);
+	  ips2Check.splice(0, 1);
 				
-				}
-				if(cids2Check.length) {
+        }
+	if(cids2Check.length) {
 				
-					cid2check = ips2Check[0];
-					connection.query('SELECT * FROM `erro_connection_log` WHERE `computerid` = ? ', [cid2check], (error, results, fields) => {
+	  cid2check = ips2Check[0];
+	  connection.query('SELECT * FROM `erro_connection_log` WHERE `computerid` = ? ', [cid2check], (error, results, fields) => {
 
-						for(var/i = 0; i < results.length; i++) {
-							var result = results[i];
+	    for(var/i = 0; i < results.length; i++) {
+	      var result = results[i];
 							
-							//let's check the ckeys first.
-							var alreadyCheckingCkey = 0;
-							for(var/j = 0; j < ckeys2Check.length; j++) {
-								if(ckeys2Check[j] == result.ckey) {
-									alreadyCheckingCkey = 1;
-									break;
-								}
-							}
-							for(var/j = 0; j < checkedCkeys.length; j++) {
-								if(checkedCkeys[j] == result.ckey) {
-									alreadyCheckingCkey = 1;
-									break;
-								}
-							}
-							if(alreadyCheckingCkey == 0) {
-								ckeys2Check.push(result.ckey);
-							}
+	      //let's check the ckeys first.
+	      var alreadyCheckingCkey = 0;
+	      for(var/j = 0; j < ckeys2Check.length; j++) {
+		if(ckeys2Check[j] == result.ckey) {
+		  alreadyCheckingCkey = 1;
+		  break;
+		}
+	      }
+	      for(var/j = 0; j < checkedCkeys.length; j++) {
+	        if(checkedCkeys[j] == result.ckey) {
+		  alreadyCheckingCkey = 1;
+		  break;
+		}
+	      }
+	      if(alreadyCheckingCkey == 0) {
+		ckeys2Check.push(result.ckey);
+	      }
 							
-							//now let's check IPs
-							var alreadyCheckingIP = 0;
-							for(var/j = 0; j < ips2Check.length; j++) {
-								if(ips2Check[j] == result.ip) {
-									alreadyCheckingIP = 1;
-									break;
-								}
-							}
-							for(var/j = 0; j < checkedIPs.length; j++) {
-								if(checkedIPs[j] == result.ip) {
-									alreadyCheckingIP = 1;
-									break;
-								}
-							}
-							if(alreadyCheckingIP == 0) {
-								ips2Check.push(result.ip);
-							}
+	      //now let's check IPs
+	      var alreadyCheckingIP = 0;
+	      for(var/j = 0; j < ips2Check.length; j++) {
+	        if(ips2Check[j] == result.ip) {
+		  alreadyCheckingIP = 1;
+		  break;
+	        }
+	      }
+	      for(var/j = 0; j < checkedIPs.length; j++) {
+	        if(checkedIPs[j] == result.ip) {
+		  alreadyCheckingIP = 1;
+		  break;
+	        }
+	      }
+	      if(alreadyCheckingIP == 0) {
+		ips2Check.push(result.ip);
+	      }
 							
-						}
+	    }
 						
-					});
-					checkedCIDs.push(ips2Check);
-					cids2Check.splice(0, 1);
+	  });
+	  checkedCIDs.push(ips2Check);
+	  cids2Check.splice(0, 1);
 				
-				}
-			}
+        }
+      }
 
     });
 		
-		var embed = new Discord.RichEmbed();
-		embed.setAuthor("Account review:", "http://i.imgur.com/GPZgtbe.png");
-		embed.addField("Found ckeys:", checkedCkeys.join(", "));
-		embed.addField("Found IPs:", checkedIPs.join(", "));
-		embed.addfield("Found CIDs:", checkedCIDs.join(", "));
-		message.channel.sendEmbed(embed);
+    var embed = new Discord.RichEmbed();
+    embed.setAuthor("Account review:", "http://i.imgur.com/GPZgtbe.png");
+    embed.addField("Found ckeys:", checkedCkeys.join(", "));
+    embed.addField("Found IPs:", checkedIPs.join(", "));
+    embed.addfield("Found CIDs:", checkedCIDs.join(", "));
+    message.channel.sendEmbed(embed);
   }
 
 }
