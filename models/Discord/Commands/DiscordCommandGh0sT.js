583985762252705ca5d4de4f3413a187717df7e7
Gh0sT2242 <34068333+Gh0sT2242@users.noreply.github.com>
2019-02-27 08:06:42 -0600

    WHERE'S MY COMMAND

---
+++ models/Discord/Commands/DiscordCommandGh0sT.js
@@ -0,0 +1,23 @@
+const DiscordCommand = require('../DiscordCommand.js');
+
+class DiscordCommandGh0sT extends DiscordCommand {
+
+   constructor(subsystem) {
+    gh0st"gh0st", "Spooky Boi", undefined, subsystem, true);
+   }
+   
+   onRun(message, permissions, args) {
+   var responses = [
+                "https://imgur.com/gallery/kLqiezk",
+                "https://imgur.com/gallery/rBt3Vgg",
+                "https://imgur.com/gallery/0ivOCgD",
+                "https://imgur.com/gallery/PoQba8K",
+                "https://imgur.com/gallery/cgJhQjz"
+      ];
+  var response = responses[Math.floor(Math.random() * responses.length)];
+    message.reply(response);
+  }
+
+}
+
+module.exports = DiscordCommandGh0sT;
