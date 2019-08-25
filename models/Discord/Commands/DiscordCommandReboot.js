983d002dd904a9870f0588b67f5fd773852c07f5
Ashleigh Carr <ashcorr20@gmail.com>
2019-08-25 23:59:50 +0100

    Changes reboot command to just reboot yogbot.

--- models/Discord/Commands/DiscordCommandReboot.js
+++ models/Discord/Commands/DiscordCommandReboot.js
@@ -1,52 +1,14 @@
 const DiscordCommand = require('../DiscordCommand.js');
-const spawn = require('child_process').spawn;
 
 class DiscordCommandReboot extends DiscordCommand {
 
   constructor(subsystem) {
-    super("reboot", "Reboot the server.", 'reboot', subsystem);
+    super("reboot", "Reboot the bot.", 'reboot', subsystem);
   }
 
   onRun(message, permissions, args) {
-    var config = this.subsystem.manager.getSubsystem("Config").config;
-    if (args.length < 1) {
-      message.reply("Available reboot commands: \n     `" + config.discord_command_character + "reboot hard` Kills the server daemon and starts a new daemon. \n     `" + config.discord_command_character + "reboot soft` tells the server daemon to restart. \n     `" + config.discord_command_character + "reboot bot` tells the bot daemon to restart and update.");
-      return;
-    }
-
-    var rebootOption = args[0];
-    this.subsystem.logger.log("info", message.author.username + "#" + message.author.discriminator + " (" + message.author.id + ") tried to reboot the server with the " + rebootOption + " reboot option.");
-
-    switch (rebootOption) {
-    case 'hard':
-      var taskkill = spawn('taskkill', ['/F', '/IM', 'dreamdaemon.exe']);
-      taskkill.on('exit', (code) => {
-        message.reply("Hard reboot exited with exit code: " + code);
-      });
-      break;
-    case 'soft':
-      var request = "?reboot";
-      byondConnector.request(request, (results) => {
-        if ('error' in results) {
-          message.channel.send(results.error);
-        }
-        else {
-          message.channel.send(results.data);
-        }
-      });
-      break;
-    case 'bot':
-      message.reply("Updating & Rebooting Bot.").then(() => {
-        this.subsystem.manager.getSubsystem("Updater").update((data) => {
-          message.channel.send(data);
-        });
-      });
-      break;
-    default:
-      message.reply("Use either the `hard`, `soft` or `bot` option");
-      break;
-
-    }
+    message.reply("Rebooting Bot.");
+    process.exit();
   }
 
 }
