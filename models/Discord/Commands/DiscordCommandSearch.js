a8557759c4c3b383052ac762e37cf666b9ce75d1
Nichlas Pihl <nichlas00100@gmail.com>
2019-12-02 23:50:56 +0000

    Update DiscordCommandSearch.js

--- models/Discord/Commands/DiscordCommandSearch.js
+++ models/Discord/Commands/DiscordCommandSearch.js
@@ -7,13 +7,14 @@ class DiscordCommandSearch extends DiscordCommand {
   }
 
   onRun(message, permissions, args) {
+    var config = this.subsystem.manager.getSubsystem("Config").config;
 
     if(args.length < 2) {
         message.channel.send("Usage is `" + config.discord_command_character + "search [yogbot/yogs] <search terms>`");
     }
     let searched = args[0].toLowerCase();
     for(var i = 1; i<args.length; i++) {
-        args[i] = encodeURIComponent(i);
+        args[i] = encodeURIComponent(args[i]);
     }
     let search = args.splice(1).join("+");
     let output;
