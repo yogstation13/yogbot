d3e9bc9f4f2663004317415420e9e9017a3f76c4
Mikail De Vera <nichtspam1@gmail.com>
2020-10-02 19:34:42 +0800

    update

--- models/Discord/Commands/DiscordCommandEggrp.js
+++ models/Discord/Commands/DiscordCommandEggrp.js
@@ -16,7 +16,7 @@ class DiscordCommandEggrp extends DiscordCommand {
 		"https://cdn.discordapp.com/attachments/458282555284783114/572244819737313312/EGG.jpg",
 		"https://cdn.discordapp.com/attachments/458282555284783114/572244843737120781/FillyEgg.jpg",
 		"https://cdn.discordapp.com/attachments/458282555284783114/572244886573809676/KitchenEGGRp.png",
-	    	"https://cdn.discordapp.com/attachments/293085045743157248/724392616984182894/unknown.png",
+	    "https://cdn.discordapp.com/attachments/293085045743157248/724392616984182894/unknown.png",
 		"https://cdn.discordapp.com/attachments/458282555284783114/572244896736608276/Hanako.jpg",
 		"https://cdn.discordapp.com/attachments/458282555284783114/572244899382951966/EggRp.png",
 		"https://cdn.discordapp.com/attachments/458282555284783114/572244911856812032/pickled-eggs-vertical-a-2000.png",
@@ -27,7 +27,11 @@ class DiscordCommandEggrp extends DiscordCommand {
 		"https://cdn.discordapp.com/attachments/458282555284783114/572244948464697356/unknown.png",
 		"https://cdn.discordapp.com/attachments/458282555284783114/572244968337440787/DannyEgg.gif",
 		"https://cdn.discordapp.com/attachments/437332037649825792/664074732731105281/IMG_20200107_044541.jpg",
-	    	"https://cdn.discordapp.com/attachments/134720091576205312/747587010256175245/egg.png"
+		"https://cdn.discordapp.com/attachments/134720091576205312/747587010256175245/egg.png",
+		//September Update Goes here
+		"https://cdn.discordapp.com/attachments/734475284446707753/756286634852352152/dreamseeker_2020-09-17_17-17-21.png",
+		"https://cdn.discordapp.com/attachments/734475284446707753/758277438764744704/image0.png",
+		"https://cdn.discordapp.com/attachments/734475284446707753/761010462766399498/unknown-24.png"
 	];
     var response = responses[Math.floor(Math.random() * responses.length)];
     message.reply(response);
