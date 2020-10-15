9ee32af81ad7d778322f08d8bc4ab1f87b53c873
jcatjmeow <40673387+jcatjmeow@users.noreply.github.com>
2020-10-14 20:35:25 -0400

    Update DiscordCommandOak.js

--- models/Discord/Commands/DiscordCommandOak.js
+++ models/Discord/Commands/DiscordCommandOak.js
@@ -9,17 +9,8 @@ class DiscordCommandOak extends DiscordCommand {
 
   onRun(message, permissions, args) {
     var responses = [
-		"https://i.imgur.com/YO4xlTm.jpg",
-		"https://i.imgur.com/S32NXxA.png",
-		"https://i.imgur.com/7XU94T2.png",
-		"https://i.imgur.com/TAY4qep.png",
-		"https://memestatic.fjcdn.com/pictures/Gondola_1ea063_6170689.jpg",
-		"https://memes4.fjcdn.com/pictures/Gondola_b416c4_6170689.jpg",
-		"https://memes4.fjcdn.com/pictures/Gondola_c27111_6170689.jpg",
-		"https://memestatic.fjcdn.com/pictures/Gondola+art_a1ac9a_6170689.jpg",
-		"https://memes4.fjcdn.com/pictures/Gondola_730f7e_6170689.jpg",
-		"http://i.imgur.com/SVwB908.png",
-		"https://i.imgur.com/ZSZzW91.png"
+		"https://i.imgur.com/BFV771G.png",
+		"https://i.imgur.com/BsfISII.png"
 	];
 	var response = responses[Math.floor(Math.random() * responses.length)];
 	const embed = new Discord.RichEmbed()
