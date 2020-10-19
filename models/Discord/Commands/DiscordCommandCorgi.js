e3cc9114076c4c46ca122eb34597828b1ff3b72d
swissloaf <39781787+swissloaf@users.noreply.github.com>
2020-10-19 10:38:05 -0500

    Update DiscordCommandCorgi.js

--- models/Discord/Commands/DiscordCommandCorgi.js
+++ models/Discord/Commands/DiscordCommandCorgi.js
@@ -13,8 +13,8 @@ class DiscordCommandCorgi extends DiscordCommand {
           "https://cdn.discordapp.com/attachments/205784753352343552/767106936965169182/y99yj67o3utz.jpg",
           "https://cdn.discordapp.com/attachments/205784753352343552/767106937166233600/kjc24vncs4811.jpg",
           "https://cdn.discordapp.com/attachments/205784753352343552/767106937358647336/cdgz8cwgovez.jpg",
-		  "https://cdn.discordapp.com/attachments/205784753352343552/767106937803374622/830Bmqj.jpg",
-		  "https://cdn.discordapp.com/attachments/205784753352343552/767106937988186122/e05qeJD.jpg",
+	  "https://cdn.discordapp.com/attachments/205784753352343552/767106937803374622/830Bmqj.jpg",
+	  "https://cdn.discordapp.com/attachments/205784753352343552/767106937988186122/e05qeJD.jpg",
           "https://cdn.discordapp.com/attachments/205784753352343552/767106937569280040/b6l73MX.jpg"
         ];
         var response = responses[Math.floor(Math.random() * responses.length)];
