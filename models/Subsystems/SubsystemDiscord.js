31fa7b2db199fe3049c16237334cefd660d966f8
nichlas0010 <nichlas0010@gmail.com>
2020-10-24 20:47:18 +0100

    Adds PR/issue recognition to Yogbot

--- models/Subsystems/SubsystemDiscord.js
+++ models/Subsystems/SubsystemDiscord.js
@@ -6,6 +6,9 @@ const DiscordForumManager = require('../Discord/DiscordForumManager.js');
 const DiscordDonorManager = require('../Discord/DiscordDonorManager.js');
 const fs = require('fs');
 const winston = require('winston');
+const ByondAPI = require('../Byond/ByondAPI.js');
+const https = require('https');
+const StringUtils = require('../Utils/String.js');
 
 class SubsystemDiscord extends Subsystem {
   constructor(manager) {
@@ -106,12 +109,19 @@ class SubsystemDiscord extends Subsystem {
       }
     }
 
-	if(message.mentions.roles.has(config.discord_jester_role)) {
-		if(!message.member.roles.has(config.discord_jester_role)) {
-			message.reply("It appears you have, for the first time, engaged in the dastardly action to ping Jester! For this crime you have been assigned the role of Jester. Congratulations on your promotion!");
-			message.member.addRole(config.discord_jester_role);
-		}
-	}
+    if(message.mentions.roles.has(config.discord_jester_role)) {
+      if(!message.member.roles.has(config.discord_jester_role)) {
+        message.reply("It appears you have, for the first time, engaged in the dastardly action to ping Jester! For this crime you have been assigned the role of Jester. Congratulations on your promotion!");
+        message.member.addRole(config.discord_jester_role);
+      }
+    }
+    
+    let prRegex = message.content.match(new RegExp("#([0-9]+)"))
+    if(prRegex) {
+      do_http("https://api.github.com/repos/yogstation13/Yogstation/pulls/" + prRegex[1]).then(response => this.postPR(JSON.parse(response), message.channel));
+      do_http("https://api.github.com/repos/yogstation13/Yogstation/issues/" + prRegex[1]).then(response => this.postIssue(JSON.parse(response), message.channel));
+    }
+
 
     if (firstCharacter === config.discord_command_character) {
       var split = content.split(" ");
@@ -202,6 +212,205 @@ class SubsystemDiscord extends Subsystem {
     var restrictedChannels = this.manager.getSubsystem("Config").config.discord_restricted_channels;
     return restrictedChannels.includes(channel);
   }
+
+  postPR(data, channel) {
+    if(data.message) {
+      return;
+    }
+    let changelog = this.compileChangelog(data)
+    let embedColor = "FF0000"; // red = closed without merge
+
+    if(data.state == "open") {
+      embedColor = "00FF00"
+    } else if(data.merged) {
+      embedColor = "9541a5"
+    }
+
+    let changelogString = "";
+    if (changelog && changelog.error) {
+      changelogString = "There was an error compilling changelog: " + changelog.error;
+    }
+    else if(changelog){
+      for (var item of changelog) {
+        changelogString += ":" + item.emoji + ":: " + item.body + "\n";
+      }
+      if (changelogString.length > 800) {
+        changelogString = "Too long to display.";
+      }
+    }
+
+    let msgTitle = data.title.replace(/</g, '')
+    let embed = new Discord.RichEmbed();
+    embed.setAuthor(msgTitle, "https://i.imgur.com/tpkgmo8.png");
+    embed.addField("Author", data.user.login, true);
+    embed.addField("Number", "#" + data.number, true);
+    embed.addField("Github Link", data.html_url, false);
+    embed.setColor(embedColor);
+
+    if (changelogString != "") {
+      embed.addField("Changelog", changelogString, false);
+    }
+
+    channel.sendEmbed(embed)
+  }
+
+  postIssue(data, channel) {
+    if(data.message || data.pull_request) {
+      return;
+    }
+
+    let embedColor = "FF0000"; // red = closed without merge
+
+    if(data.state == "open") {
+      embedColor = "00FF00"
+    }
+
+    let msgTitle = data.title.replace(/</g, '')
+    let embed = new Discord.RichEmbed();
+    embed.setAuthor(msgTitle, "https://i.imgur.com/tpkgmo8.png");
+    embed.addField("Author", data.user.login, true);
+    embed.addField("Number", "#" + data.number, true);
+    embed.addField("Github Link", data.html_url, false);
+    embed.setColor(embedColor);
+    channel.sendEmbed(embed)
+  }
+
+  compileChangelog(data) {
+    var body = StringUtils.replaceAll(data.body, "\r\n", "\n"); // Remove github line endings
+    body = body.split("\n"); // Break the string into an array of lines
+
+    var inCLTag = false;
+    var changelog = [];
+    var username = data.user.login;
+
+    // Variables for catching changelog errors
+    var foundOpeningTag = false;
+    var foundClosingTag = false;
+
+    for (var line of body) {
+      line = line.trim();
+      if (line.substring(0, 4) == ":cl:" || line.substring(0, 4) == "🆑") {
+        inCLTag = true;
+        foundOpeningTag = true;
+
+        if (line.length > 4) {
+          if (line.charAt(4) != " ") {
+            return { error: "Changelog author field is incorrect." };
+          }
+        }
+
+        var nameSpace = line.indexOf(" ");
+
+        if (nameSpace > -1) {
+          username = line.substr(nameSpace);
+        }
+
+        continue;
+      }
+      else if (line.substring(0, 5) == "/:cl:" || line.substring(0, 6) == "/ :cl:" || line.substring(0, 5) == ":/cl:" || line.substring(0, 5) == "/🆑" || line.substring(0, 6) == "/ 🆑" || line.substring(0, 5) == ":/🆑") {
+        if (!inCLTag) {
+          return { error: "Found end of changelog before beginning" };
+        }
+        inCLTag = false;
+        foundClosingTag = true;
+        continue;
+      }
+
+      if (!inCLTag) {
+        continue;
+      }
+
+      var changelogType = line.split(" ")[0];
+      var changelogText = "";
+      var spacePos = line.indexOf(" ");
+
+      if (nameSpace) {
+        changelogType = line.substring(0, spacePos);
+        changelogText = line.substring(spacePos + 1);
+      }
+      else {
+        changelogText = line;
+      }
+
+      if (!changelogType.length || changelogType.charAt(changelogType.length - 1) != ":") {
+        continue;
+      }
+
+      var cltype = changelogType.substring(0, changelogType.length - 1);
+
+      switch (cltype) {
+        case 'author':
+          username = changelogText;
+          break;
+        case 'fix':
+        case 'fixes':
+        case 'bugfix':
+          changelog.push({ 'type': 'bugfix', 'emoji': 'bug', 'body': changelogText });
+          break;
+        case 'wip':
+          changelog.push({ 'type': 'wip', 'emoji': 'biohazard', 'body': changelogText });
+          break;
+        case 'rsctweak':
+        case 'tweaks':
+        case 'tweak':
+          changelog.push({ 'type': 'tweak', 'emoji': 'wrench', 'body': changelogText });
+          break;
+        case 'soundadd':
+          changelog.push({ 'type': 'soundadd', 'emoji': 'loud_sound', 'body': changelogText });
+          break;
+        case 'sounddel':
+          changelog.push({ 'type': 'sounddel', 'emoji': 'mute', 'body': changelogText });
+          break;
+        case 'add':
+        case 'adds':
+        case 'rscadd':
+          changelog.push({ 'type': 'rscadd', 'emoji': 'battery', 'body': changelogText });
+          break;
+        case 'del':
+        case 'dels':
+        case 'rscdel':
+          changelog.push({ 'type': 'rscdel', 'emoji': 'octagonal_sign', 'body': changelogText });
+          break;
+        case 'imageadd':
+          changelog.push({ 'type': 'imageadd', 'emoji': 'art', 'body': changelogText });
+          break;
+        case 'imagedel':
+          changelog.push({ 'type': 'imagedel', 'emoji': 'scissors', 'body': changelogText });
+          break;
+        case 'typo':
+        case 'spellcheck':
+          changelog.push({ 'type': 'spellcheck', 'emoji': 'pen_ballpoint', 'body': changelogText });
+          break;
+        case 'experimental':
+        case 'experiment':
+          changelog.push({ 'type': 'experiment', 'emoji': 'biohazard', 'body': changelogText });
+          break;
+        case 'tgs':
+          changelog.push({ 'type': 'tgs', 'emoji': 'question', 'body': changelogText });
+          break;
+        default:
+          return { error: cltype + " is not a recognized changelog option" };
+      }
+    }
+    return changelog
+  }
+}
+
+function do_http(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, {headers: {
+      "User-Agent": "Yogbot"
+    }} , (res) => {
+      if(res.statusCode == 200) {
+        res.setEncoding('utf8');
+        let data = '';
+        res.on('data', (chunk) => {data += chunk;});
+        res.on('end', () => {
+          resolve(data);
+        })
+      }
+    });
+  });
 }
 
 module.exports = SubsystemDiscord;
