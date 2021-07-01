b0ba087a82d9025836a83975434753226519087d
JC Denthead <63614296+MaintClown@users.noreply.github.com>
2021-07-01 07:08:42 -0400

    Adds a YouTube link to !jamie.

--- models/Discord/Commands/DiscordCommandJamie.js
+++ models/Discord/Commands/DiscordCommandJamie.js
@@ -44,6 +44,7 @@ class DiscordCommandJamie extends DiscordCommand {
         "https://cdn.discordapp.com/attachments/205784753352343552/768561221300387870/unknown.png",
         "https://cdn.discordapp.com/attachments/734475284446707753/768887587988832307/unknown.png",
 	"https://cdn.discordapp.com/attachments/860107308574965760/860107324009742336/unknown-99.png",
+	"https://www.youtube.com/watch?v=o6Fkov-Nrw8",
         "https://cdn.discordapp.com/attachments/734475284446707753/772700667969011712/unknown.png"
         ];
         var response = responses[Math.floor(Math.random() * responses.length)];
