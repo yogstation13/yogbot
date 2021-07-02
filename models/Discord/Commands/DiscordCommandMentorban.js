024e0e012aeb7b90572c8833af130f2439cf52ec
LoliconSlayer <54045237+LoliconSlayer@users.noreply.github.com>
2021-07-01 23:32:00 -0700

    Create DiscordCommandMentorban.js
    Papi is now responsible for the creation of two ban roles/commands

---
+++ models/Discord/Commands/DiscordCommandMentorban.js
@@ -0,0 +1,34 @@
+const DiscordCommand = require('../DiscordCommand.js');	
+
+class DiscordCommandMentorban extends DiscordCommand {	
+
+    constructor(subsystem) {	
+        super("mentorban", "Ban someone from the mentor channel.", 'mentorban', subsystem);	
+    }	
+
+    onRun(message, permissions, args) {	
+        let config = this.subsystem.manager.getSubsystem("Config").config;	
+        let role = config.discord_mentorban_role;
+        let member = message.mentions.members.first();
+
+        if(member == undefined) {
+          message.reply("I could not find that user, Make sure you use the mention format of @Username");
+          return;
+        }
+
+        if (member.roles.has(config.discord_mentorban_role)) {	
+            member.removeRole(config.discord_mentorban_role);	
+            var response = "User was un-mentorbanned."	
+        }	
+        else {	
+            member.addRole(config.discord_mentorban_role);	
+            var response = "User was mentorbanned."	
+        }	
+
+        message.channel.send(response);	
+    }	
+
+}	
+
+module.exports = DiscordCommandMentorban;
+
