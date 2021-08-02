6bafed95be7440d50190b2c8a4808313eccb5a37
LoliconSlayer <54045237+LoliconSlayer@users.noreply.github.com>
2021-08-02 17:48:14 -0400

    Forgot a comma

--- models/Discord/Commands/DiscordCommandCouncil.js
+++ models/Discord/Commands/DiscordCommandCouncil.js
@@ -10,7 +10,7 @@ class DiscordCommandCouncil extends DiscordCommand {
     onRun(message, permissions, args) {
         var responses = [
 		"https://i.imgur.com/8PQmYdL.png",
-		"https://cdn.discordapp.com/attachments/475835667788857375/773148718311931904/unknown.png"
+		"https://cdn.discordapp.com/attachments/475835667788857375/773148718311931904/unknown.png",
 		"https://cdn.discordapp.com/attachments/734475284446707753/826240137225830400/image0.png", //Ashcorr gets fooled by Morderhel
 		"https://cdn.discordapp.com/attachments/734475284446707753/804697809323556864/unknown.png", //Get a sense of humor
 		"https://cdn.discordapp.com/attachments/734475284446707753/804192496322084864/ban.png", //Banned by public vote
