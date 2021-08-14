272f7efc30b90be327e11a78923102aabb4d9e6c
TheGamerdk <5618080+TheGamerdk@users.noreply.github.com>
2021-08-14 17:36:55 +0200

    Update DiscordChannelAsay.js

--- models/Discord/Channels/DiscordChannelAsay.js
+++ models/Discord/Channels/DiscordChannelAsay.js
@@ -12,14 +12,17 @@ class DiscordChannelAsay extends DiscordChannel {
     var byondConnector = this.subsystem.manager.getSubsystem("Byond Connector").byondConnector;
     var config = this.subsystem.manager.getSubsystem("Config").config;
     var data = striptags(message.content)
-	
+    var guildMember = message.guild.fetchMember(message.author)
+    
+    
     message.attachments.forEach((image) => {
 		if(image && (image.filename.endsWith(".jpg") || image.filename.endsWith(".png"))){
 			data = data+"<br><img src=\""+image.url+"\" alt=\"Image\">";
 		}
 	});
-
-    byondConnector.request("?asay=" + encodeURIComponent(data) + "&admin=" + encodeURIComponent(message.author.username + "/" + message.author.displayName), (results) => {
+	
+  
+    byondConnector.request("?asay=" + encodeURIComponent(data) + "&admin=" + encodeURIComponent(message.author.username + "/" + guildMember.displayName), (results) => {
       if ('error' in results) {
         message.channel.send(results.error);
       }
