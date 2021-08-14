ce4a8e8756bd95c9b4f57d1ce9fbf07d073b0651
Jamie D <993128+JamieD1@users.noreply.github.com>
2021-08-14 23:07:43 +0100

    Fixes

--- models/Discord/Commands/DiscordCommandListAdmins.js
+++ models/Discord/Commands/DiscordCommandListAdmins.js
@@ -9,7 +9,7 @@ class DiscordCommandListAdmins extends DiscordCommand {
 
     onRun(message, permissions, args) {
         var config = this.subsystem.manager.getSubsystem("Config").config;
-        if (args.length < 1) {
+        if (args.length != 2) {
             message.reply("Usage is `" + config.discord_command_character + "listadmins`");
             return;
         }
