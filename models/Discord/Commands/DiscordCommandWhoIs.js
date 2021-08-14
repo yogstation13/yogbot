16e75858c2e22392a71e2171d4e1b38c327df1b4
TheGamerdk <5618080+TheGamerdk@users.noreply.github.com>
2021-08-14 16:59:32 +0200

    Update DiscordCommandWhoIs.js

--- models/Discord/Commands/DiscordCommandWhoIs.js
+++ models/Discord/Commands/DiscordCommandWhoIs.js
@@ -78,11 +78,8 @@ function getByCkey(connection, ckey, message) {
         message.reply("More than 1 of this ckey with a Discord ID, this makes no sense at all!");
         return;
       }
-      message.reply(ckey + " belongs to <@" + results[0].discord_id + ">")
-      message.reply(JSON.stringify(results))
       message.guild.fetchMember(results[0].discord_id)
-        .then((member) => message.reply(ckey + " belongs to " + member.user.username + "#" + member.user.discriminator))
-        .catch(() => message.reply("Can't resolve user from ID"));
+        .then((member) => message.reply(ckey + " belongs to " + member.user.username + "#" + member.user.discriminator));
     })
 }
 
