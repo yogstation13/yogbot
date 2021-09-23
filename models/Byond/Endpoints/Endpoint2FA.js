b9982cf32c1060800ab5fd9a31c87c415c011b9f
Gabriel Adamson <adamson.g@gmail.com>
2021-09-23 16:48:00 -0500

    Fixes some errors, makes the code work

--- models/Byond/Endpoints/Endpoint2FA.js
+++ models/Byond/Endpoints/Endpoint2FA.js
@@ -7,113 +7,88 @@ class EndpointMFA extends APIEndpoint {
 	}
 
 	request(data, callback) {
-	
     var config = this.manager.subsystemManager.getSubsystem("Config").config;
     var discord = this.manager.subsystemManager.getSubsystem("Discord");
     var dbSubsystem = this.manager.subsystemManager.getSubsystem("Database");
 
-    var error;
-    var user_id;
+    var error = null;
+    var user_id = null;
 
     dbSubsystem.pool.getConnection((err, connection) => {
       if (err) {
         connection.release();
-        error = "Error contacting database.";
         return;
       }
 
-      user_id = getByCkey(connection, data.ckey);
+      connection.query("SELECT discord_id FROM `erro_player` WHERE `ckey` = ?", [data.ckey], (error, results, fields) => {
+        if (error) {
+          this.manager.subsystemManager.logger.log("error", error)
+          return;
+        }
+        if (results.length == 0) {
+          this.manager.subsystemManager.logger.log("error", "No associated ID")
+          return;
+        }
+        if (results.length > 1) {
+          this.manager.subsystemManager.logger.log("error", "Multiple associated IDs")
+          return;
+        }
+        user_id = results[0].discord_id;
 
-      if (user_id === null)
-        error = "User ID not linked!"
+        discord.getPrimaryGuild().fetchMember(user_id)
+          .then((member) => {
+            var embed = new Discord.RichEmbed();
+            embed.setDescription("New connection for <@" + user_id + "> please confirm this is you.");
+            embed.addField("IP Address", data.ip, true);
+            embed.addField("CID", data.cid, true);
+            embed.addField("CKey", data.ckey, true);
+            for (var channel of discord.getPrimaryGuild().channels.array()) {
+              if (channel.id == config.discord_channel_multi_factor) {
+                channel.send(embed).then(message => {
+                  message.react("👍").then(() =>
+                    message.react("👎").then(() => {
+                      const filter = (reaction, user) => {
+                        return ["👍", "👎"].includes(reaction.emoji.name) && user.id === user_id
+                      }
+                      message.awaitReactions(filter, {max: 1, time: 60000}).then(collected => {
+                        const reaction = collected.first();
+
+                        if(reaction && reaction.emoji.name == "👍") {
+                          var byondConnector = this.manager.subsystemManager.getSubsystem("Byond Connector").byondConnector;
+                          byondConnector.request("?mfa_verify&ckey=" + encodeURIComponent(data.ckey) + "&ip=" + encodeURIComponent(data.ip) + "&cid=" + encodeURIComponent(data.cid), (results) => {
+                            if ('error' in results) {
+                              message.channel.send(results.error);
+                            }
+                          });
+                        }
+                      });
+                    })
+                  )
+                });
+              }
+            }
+          })
+          .catch(() => {
+            for (var channel of discord.getPrimaryGuild().channels.array()) {
+              if (channel.id == config.discord_channel_multi_factor) {
+                channel.send("**" + data.ckey + " MFA error**: Unable to find user " + user_id + " in guild.");
+              }
+            }
+          });
+
+      })
 
       connection.release();
     });
 
-    var response;
-
-    if(error !== null) {
-      for (var channel of discord.getPrimaryGuild().channels.array()) {
-        if (channel.id == config.discord_channel_ooc) {
-          channel.send("**" + data.ckey + " MFA error**: " + error);
-        }
-      }
-      response = {
-        status: 500,
-        response: error
-      }
-      return callback(undefined, response)
-    }
-
-    discord.getPrimaryGuild().guild.fetchMember(user_id)
-        .then((member) => {
-          var embed = new Discord.RichEmbed();
-          embed.setDescription("New connection for " + member.mention + " please confirm this is you.");
-          embed.addField("IP Address", data.ip, true);
-          embed.addField("CID", data.cid, true);
-          embed.addField("CKey", data.ckey, true);
-          for (var channel of discord.getPrimaryGuild().channels.array()) {
-            if (channel.id == config.discord_channel_ooc) {
-              channel.sendEmbed(embed).then(message => {
-                message.react("<:Approve:731199508066599085>").then(() =>
-                  message.react("<:Disapprove:731199523074080820>").then(() => {
-                    const filter = (reaction, user) => {
-                      return ["<:Approve:731199508066599085>", "<:Disapprove:731199523074080820>"].includes(reaction.emoji.name) && user.id === user_id
-                    }
-                    message.awaitReactions({filter, max: 1, time: 60000}).then(collected => {
-                      const reaction = collected.first();
-
-                      if(reaction.emoji.name == "<:Approve:731199508066599085>") {
-                        var byondConnector = this.manager.subsystemManager.getSubsystem("Byond Connector").byondConnector;
-                        byondConnector.request("?mfa_verify&ckey=" + encodeURIComponent(data.ckey) + "&ip=" + encodeURIComponent(data.ip) + "&cid=" + encodeURIComponent(data.cid), (results) => {
-                          if ('error' in results) {
-                            message.channel.send(results.error);
-                          }
-                        });
-                      } else {
-                        // TODO: Handle negative resposne
-                      }
-                    });
-                  })
-                )
-              });
-            }
-          }
-        })
-        .catch(() => {
-          for (var channel of discord.getPrimaryGuild().channels.array()) {
-            if (channel.id == config.discord_channel_ooc) {
-              channel.send("**" + data.ckey + " MFA error**: Unable to find user in guild.");
-            }
-          }
-          response = {
-            status: 500,
-            response: "Unable to find user in guild."
-          }
-          return callback(undefined, response)
-        });
-
-    response = {
+    var response = {
       status: 200,
       response: "Verification sent."
-    }
+    };
+
     return callback(undefined, response);
 	}
+
 }
 
 module.exports = EndpointMFA;
-
-function getByCkey(connection, ckey) {
-	connection.query("SELECT discord_id FROM `erro_player` WHERE `ckey` = ?", [ckey], (error, results, fields) => {
-		if (error) {
-			return;
-		}
-		if (results.length == 0) {
-			return;
-		}
-		if (results.length > 1) {
-			return;
-		}
-		return results[0].discord_id;
-  })
-}
