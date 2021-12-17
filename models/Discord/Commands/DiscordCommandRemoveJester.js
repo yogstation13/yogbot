025dc8ebd2df8c3e7eef5da1eb9c2ca3f1c2a5cf
TheGamerdk <5618080+TheGamerdk@users.noreply.github.com>
2021-12-17 17:46:17 +0100

    Update DiscordCommandRemoveJester.js

--- models/Discord/Commands/DiscordCommandRemoveJester.js
+++ models/Discord/Commands/DiscordCommandRemoveJester.js
@@ -8,6 +8,8 @@ class DiscordCommandRemoveJester extends DiscordCommand {
 
   onRun(message, permissions, args) {
     var config = this.subsystem.manager.getSubsystem("Config").config;
+    if(message.member.id == 315319488767852545)
+      return
     if(message.member.roles.has(config.discord_jester_role)) {
       message.member.removeRole(config.discord_jester_role);
       message.reply("Success! But beware if you violate the sacred Jester Oath by daring to ping Jester once again you shall be smited with a thousand YOGGERS!")
