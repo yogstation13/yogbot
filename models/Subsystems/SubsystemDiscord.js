b9ff40ceed6d6517891b27291bcec89414db87c0
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2022-03-29 17:18:26 -0400

    Update SubsystemDiscord.js

--- models/Subsystems/SubsystemDiscord.js
+++ models/Subsystems/SubsystemDiscord.js
@@ -1,10 +1,8 @@
 const Subsystem = require('../Subsystem.js');
 const Discord = require('discord.js');
 const DiscordPermissionManager = require('../Discord/DiscordPermissionManager.js');
-const DiscordBanManager = require('../Discord/DiscordBanManager.js');
 const DiscordForumManager = require('../Discord/DiscordForumManager.js');
 const DiscordDonorManager = require('../Discord/DiscordDonorManager.js');
-const DiscordStickyRoleManager = require('../Discord/DiscordStickyRoleManager.js');
 const fs = require('fs');
 const winston = require('winston');
 const https = require('https');
@@ -15,10 +13,8 @@ class SubsystemDiscord extends Subsystem {
     super("Discord", manager);
     this.client = new Discord.Client();
     this.permissionManager = new DiscordPermissionManager(manager);
-    this.banManager = new DiscordBanManager(this);
     this.forumManager = new DiscordForumManager(this);
     this.donorManager = new DiscordDonorManager(this);
-    this.stickyRoleManager = new DiscordStickyRoleManager(this);
     this.oauthState = new Map();
     this.logger;
 
@@ -34,10 +30,8 @@ class SubsystemDiscord extends Subsystem {
     var config = this.manager.getSubsystem("Config").config;
     this.client.login(config.discord_token).then(atoken => {
       this.loadCommands();
-      this.banManager.setup();
       this.forumManager.setup();
       this.donorManager.setup();
-      this.stickyRoleManager.setup();
 	this.client.user.setGame("I AM GOD");
       callback();
     }).catch((err) => {
